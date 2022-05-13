89f9ef2bded891ded3c26a1d9e3a67a93b690da6
houssam-dev <developpeur.pro.ma@gmail.com>
2022-05-13 19:02:18 +0100

    refactor: add dynamic counter functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,41 +1,73 @@
-function loadTsFormData() {
-	console.log(`📡 loadTSFormData`);
-	const alertNotif = document.querySelector('.alert');
-	const closeBtn = document.querySelector('.close-btn');
-	const tsForm = document.querySelector('#myForm');
-	tsForm.addEventListener('submit', (event) => {
-		event.preventDefault();
+const tsForm = document.querySelector('#myForm');
+const alertNotif = document.querySelector('.alert');
+const closeBtn = document.querySelector('.close-btn');
+//
+const btnMinusBag = document.querySelector('.btnMinusBag');
+const counterBagElement =  document.querySelector('.counterBag');
+const btnPlusBag = document.querySelector('.btnPlusBag');
 
-		// const formData = new FormData(tsForm);
-		// console.log(`🚧 loadTsFormData() #formData `, formData);
+const btnMinusShoes = document.querySelector('.btnMinusShoes');
+const counterShoesElement =  document.querySelector('.counterShoes');
+const btnPlusShoes = document.querySelector('.btnPlusShoes');
 
-		// Array.from(formData.entries()).forEach(it => console.log(`|__ 🚧 loadTsFormData() #entry `, it))
-		const inputs = document.getElementsByTagName('input');
+let counterShoes = 1;
+let counterBag = 1;
 
-		const controls = Array.from(inputs);
-		controls.forEach( (it, index) => console.log(`🚀 ${index} > ${it.name} ${it.checkValidity()} #value: ${it.value}`));
+tsForm.addEventListener('submit', (event) => {
+	event.preventDefault();
 
-		const res = controls.every( (it) => it.value !== '' && it.checkValidity());
-		
-		if (res) {
-			alertNotif.classList.remove('hidden');
-			alertNotif.classList.add("show");
-			alertNotif.classList.remove("hide");
-			alertNotif.classList.add("showAlert");
-			setTimeout(function () {
-			  alertNotif.classList.remove("show");
-			  alertNotif.classList.add("hide");
-			}, 5000);
-		}
+	// const formData = new FormData(tsForm);
+	// console.log(`🚧 loadTsFormData() #formData `, formData);
 
-	}, false);
+	// Array.from(formData.entries()).forEach(it => console.log(`|__ 🚧 loadTsFormData() #entry `, it))
+	const inputs = document.getElementsByTagName('input');
 
+	const controls = Array.from(inputs);
+	controls.forEach( (it, index) => console.log(`🚀 ${index} > ${it.name} ${it.checkValidity()} #value: ${it.value}`));
+
+	const res = controls.every( (it) => it.value !== '' && it.checkValidity());
 	
-	closeBtn.addEventListener("click", function (event) {
-		alertNotif.classList.remove("show");
-		alertNotif.classList.add("hide");
-	});
-	
-}
+	if (res) {
+		alertNotif.classList.remove('hidden');
+		alertNotif.classList.add("show");
+		alertNotif.classList.remove("hide");
+		alertNotif.classList.add("showAlert");
+		setTimeout(function () {
+		  alertNotif.classList.remove("show");
+		  alertNotif.classList.add("hide");
+		}, 5000);
+	}
 
-loadTsFormData(); 
+}, false);
+
+
+closeBtn.addEventListener("click", function (event) {
+	alertNotif.classList.remove("show");
+	alertNotif.classList.add("hide");
+});
+
+
+
+btnMinusBag.addEventListener('click', event => {
+	event.preventDefault();
+	if (counterBag < 1) { counterBagElement.textContent = '0'; return;}
+
+	counterBagElement.textContent = --counterBag;
+}, false);
+
+btnPlusBag.addEventListener('click', event => {
+	event.preventDefault();
+	counterBagElement.textContent = ++counterBag;
+}, false);
+
+btnMinusShoes.addEventListener('click', event => {
+	event.preventDefault();
+	if (counterShoes < 1) { counterShoesElement.textContent = '0'; return;}
+
+	counterShoesElement.textContent = --counterShoes;
+}, false);
+
+btnPlusShoes.addEventListener('click', event => {
+	event.preventDefault();
+	counterShoesElement.textContent = ++counterShoes;
+}, false);
